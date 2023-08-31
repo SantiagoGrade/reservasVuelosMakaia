a7c91ff5e8b73356b33a3031400795080dac2d74
Sebastian mendez <91163094+Mendeh1@users.noreply.github.com>
2023-08-31 07:38:43 -0500

    Add files via upload
    base de la base de la base #1

---
+++ Vuelos.java
@@ -0,0 +1,9 @@
+import java.time.*;
+public class Vuelos {
+    private int codigo;
+    private  String origen;
+    private String destino;
+    private double precio;
+    private  int asientos_disponibles;
+
+}
